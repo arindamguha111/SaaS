6bc2b33d48ca455e308a57f1cd5a28452163438a
A G <arindamguha1111.com>
2025-03-17 13:03:45 +0530

    added views

--- src/shome/urls.py
+++ src/shome/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_view
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view)
+    path('', base_view),
+    path('home/', home)
 ]
--- src/shome/views.py
+++ src/shome/views.py
@@ -5,10 +5,19 @@ from django.shortcuts import render
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_view(request):
+def base_view(request):
     context = {
-        "title" : "SaaS | Home",
-        "name" : "IDIOT"
+        "title" : "SaaS",
+        "message" : "Welcome To Django"
+    }
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return render(request, "base.html", context)
+
+def home(request):
+    context = {
+        "title" : "Homepage",
+        "message" : "Welcome to the Home Page"
     }
     # html_file_path = this_dir / "home.html"
     # html_ = html_file_path.read_text()
